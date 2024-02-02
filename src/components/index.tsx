4fc2541f780695ffb12335f7f93dbbac6fe89d03
Mike Purvis <mikedotexe@gmail.com>
2024-02-01 22:02:28 -0800

    move modal logic into component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -17,8 +17,46 @@ export default function BosMain() {
     const signedIn = useAuthStore((store) => store.signedIn);
     const walletModal = useAuthStore((store) => store.walletModal);
 
+    const openNEARWalletSelector = () => {
+        if (walletModal && typeof walletModal.show === 'function') {
+            walletModal.show();
+        } else {
+            console.error('walletModal is not initialized :/');
+        }
+    }
+
     return (
         <Container>
+            <div id={"very-top"} style={{
+                position: 'fixed',
+                top: 0,
+                right: 0,
+                padding: '10px',
+            }}>
+                {(!signedIn) && (
+                    <>
+                        <button style={{
+                            backgroundColor: '#00ec97',
+                            border: 'none',
+                            color: 'white',
+                            padding: '10px 20px',
+                            textTransform: 'uppercase',
+                            letterSpacing: '3px',
+                            fontWeight: "bold",
+                            cursor: 'pointer',
+                            boxShadow: '0 6px 8px rgba(0, 0, 0, 0.3)',
+                            filter: 'brightness(83%)',
+                        }} onClick={() => openNEARWalletSelector()}>Login
+                        </button>
+                    </>
+                )}
+                {/*      TODO: Create some delete key log-out functionality here.*/}
+                {/*        {signedIn && (*/}
+                {/*            <>*/}
+                {/*                <p>logged in</p>*/}
+                {/*            </>*/}
+                {/*        )}*/}
+            </div>
             <VmComponent
                 src={widget}
                 props={{hideDisclaimer: true}}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,17 +10,10 @@ import BosMain from '@/components';
 const HomePage: NextPageWithLayout = () => {
   const signedIn = useAuthStore((store) => store.signedIn);
   const walletModal = useAuthStore((store) => store.walletModal);
+  // const walletModal = useAuthStore((store) => store.requestSignInWithWallet);
   // the below didn't seem to work
   // const requestSignInWithWallet = useAuthStore((store) => store.requestSignInWithWallet);
   const setComponentSrc = useCurrentComponentStore((store) => store.setSrc);
-
-  const openNEARWalletSelector = () => {
-      if (walletModal && typeof walletModal.show === 'function') {
-          walletModal.show();
-      } else {
-          console.error('walletModal is not initialized :/');
-      }
-  }
 
   useEffect(() => {
     if (!signedIn) {
@@ -31,38 +24,9 @@ const HomePage: NextPageWithLayout = () => {
   return (
       <>
           <MetaTags
-              title={`BOS Gateway Template`}
-              description={`"NEAR isn’t just a Layer 1 blockchain — it’s the Blockchain Operating System for an  Open Web. Create and discover decentralized apps, and help build the future of the web, today."`}
+              title={`Staking NEAR`}
+              description={`"Stake, unstake, withdraw NEAR using verifiable frontend component code."`}
           />
-          <div id={"very-top"} style={{
-              position: 'fixed',
-              top: 0,
-              right: 0,
-              padding: '10px',
-          }}>
-              {(!signedIn) && (
-                  <>
-                      <button style={{
-                          backgroundColor: '#00ec97',
-                          border: 'none',
-                          color: 'white',
-                          padding: '10px 20px',
-                          textTransform: 'uppercase',
-                          letterSpacing: '3px',
-                          fontWeight: "bold",
-                          cursor: 'pointer',
-                          boxShadow: '0 6px 8px rgba(0, 0, 0, 0.3)',
-                          filter: 'brightness(83%)',
-                      }} onClick={() => openNEARWalletSelector()}>Login</button>
-                  </>
-              )}
-              {/*      TODO: Create some delete key log-out functionality here.*/}
-              {/*        {signedIn && (*/}
-              {/*            <>*/}
-              {/*                <p>logged in</p>*/}
-              {/*            </>*/}
-              {/*        )}*/}
-          </div>
           <BosMain/>
       </>
   );
