e5674ec81dd4bfc94a5f616d638f93a6082409e8
Mike Purvis <mikedotexe@gmail.com>
2024-02-01 21:11:13 -0800

    trying something for Vercel

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   output: 'export',
   compiler: { styledComponents: true },
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Disable minification
+    if (!dev) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
