808958c6d1edb0b29714239de9b144fcc6630a8d
Mike Purvis <mikedotexe@gmail.com>
2024-02-02 16:43:04 -0800

    add ability to set URL parameter to enable bosLoaderUrl flag, add local URL param as shortcut

--- src/hooks/useFlags.ts
+++ src/hooks/useFlags.ts
@@ -20,6 +20,30 @@ export function useFlags() {
     setRawFlags(flags);
   }, []);
 
+  useEffect(() => {
+    // Existing flags from local storage or an empty object
+    let flags = localStorage.getItem('flags') ? JSON.parse(localStorage.getItem('flags') || '') : {};
+
+    // Parse URL parameters
+    const params = new URLSearchParams(window.location.search);
+    const bosLoaderUrl = params.get('bosLoaderUrl');
+    const hasLocalURLParam = params.has('local');
+
+    // Check if URL contains bosLoaderUrl and update flags accordingly
+    if (bosLoaderUrl) {
+      flags = { ...flags, bosLoaderUrl };
+      setRawFlags(flags);
+    } else if (hasLocalURLParam) {
+        const whatThatMeans = {
+          bosLoaderUrl: "http://127.0.0.1:3030"
+        };
+        flags = { ...flags, ...whatThatMeans };
+        setRawFlags(flags);
+    }
+
+  }, []);
+
+
   const setFlags = useCallback((newFlags: Flags) => {
     setRawFlags((f) => {
       const updated = { ...f, ...newFlags };
