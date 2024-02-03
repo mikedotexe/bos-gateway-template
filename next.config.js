b6da81eca52a838dd44c4554537bc0ba622b31bf
Mike Purvis <mikedotexe@gmail.com>
2024-02-02 18:46:15 -0800

    modify next config, minimization, comment out stop the output

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   compiler: { styledComponents: true },
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Disable minification
-    if (!dev) {
-      config.optimization.minimize = false;
-    }
-    return config;
-  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   // Disable minification
+  //   if (!dev) {
+  //     config.optimization.minimize = false;
+  //   }
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
