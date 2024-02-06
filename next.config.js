e38a27ffc6068ec387eeef024f242121dec188b3
Mike Purvis <mikedotexe@gmail.com>
2024-02-05 16:55:30 -0800

    add helpful comments in next.config.js about Tauri vs. NextJS

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Commented the "output" key below for NextJS to be able to do "next start"
+  // But Tauri seems to want this: https://tauri.app/v1/guides/getting-started/setup/next-js/#nextjs-static-exports
+  // But I'm wondering how necessary that is with a custom VM?
   // output: 'export',
   compiler: { styledComponents: true },
   reactStrictMode: true,
